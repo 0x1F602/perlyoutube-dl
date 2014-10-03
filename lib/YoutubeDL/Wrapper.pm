3e37c010d5e18950a2d6b77ec48584752abce8c3
pat <pat@pat-1201N.(none)>
2014-10-02 21:10:20 -0400

    Added Wrapper object config

--- lib/YoutubeDL/Wrapper.pm
+++ lib/YoutubeDL/Wrapper.pm
@@ -1,6 +1,38 @@
 package YoutubeDL::Wrapper 1.0;
 use Moose;
-use Inline::Python;
+use YoutubeDL::Wrapper::Config;
 
-__PACKAGE__->meta->make_immutable;
+has 'config' => (
+    is => 'rw',
+    builder => '_get_config',
+    lazy => 1,
+);
+
+sub _get_config {
+    my ($self) = @_;
+    my $config = YoutubeDL::Wrapper::Config->new();
+    return $config;
+}
+
+sub run {
+    my ($self, $command) = @_;
+    my $output = {
+        stdout => '',
+        stderr => '',
+    };
+    my $process = IPC::Open3::Simple->new(
+        out => sub {
+            my $line = shift;
+            $output->{stdout} .= "$line\n";
+        },
+        err => sub {
+            my $line = shift;
+            $output->{stderr} .= "$line\n";
+        }
+    );
+    my  $retval = $process->run($command);
+    return $output;
+}
+
+#__PACKAGE__->meta->make_immutable;
 1;
