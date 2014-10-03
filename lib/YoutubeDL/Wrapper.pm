35fab51e67dc06154e3c11485bcf7cbd1636b390
pat <pat@pat-1201N.(none)>
2014-10-03 00:57:20 -0400

    Fixed the circular loading issue

--- lib/YoutubeDL/Wrapper.pm
+++ lib/YoutubeDL/Wrapper.pm
@@ -1,12 +1,13 @@
 package YoutubeDL::Wrapper 1.0;
 use Moose;
 use YoutubeDL::Wrapper::Config;
+use IPC::Open3::Simple;
 
 has 'config_filename' => (
     is => 'rw',
-    #default => 'wrong.yml',
+    default => 'config.yml',
     isa => 'Str',
-    #lazy => 1,
+    lazy => 1,
     writer => 'set_config_filename',
 );
 
@@ -16,16 +17,34 @@ has 'config' => (
     lazy => 1,
 );
 
+has 'executable_version' => (
+    is => 'ro',
+    builder => '_get_executable_version',
+    lazy => 1,
+);
+
+
 #sub set_config_filename {
 #    my ($self, $fn) = @_;
 #    warn "Filename $fn";
 #    #$self->config_filename = $fn;
 #    return $fn;
 #}
+sub _get_executable_version {
+    my ($self) = @_;
+    my $observed_version = {};
+    my $executable = $self->config->executable;
+    my $output = $self->run("$executable -v");
+    my @matches = $output->{stderr} =~ 
+        m/\[debug\] youtube-dl version (?<year>\d\d\d\d).(?<month>\d\d).(?<day>\d\d).(?<release>\d+)/;
+    if (scalar @matches == 4) {
+        $observed_version->{$_} = shift @matches for qw/year month day release/;
+    }
+    return $observed_version;
+}
 
 sub _get_config {
     my ($self) = @_;
-
     warn $self->config_filename;
     my $config = YoutubeDL::Wrapper::Config->new(config_filename => $self->config_filename);
     return $config;
--- lib/YoutubeDL/Wrapper/Config.pm
+++ lib/YoutubeDL/Wrapper/Config.pm
@@ -1,8 +1,14 @@
 package YoutubeDL::Wrapper::Config;
 use Moose;
-extends 'YoutubeDL::Wrapper';
 
 use YAML::XS qw/LoadFile/;
+
+has 'config_filename' => (
+    is => 'rw',
+    default => 'config.yml',
+    isa => 'Str',
+    lazy => 1,
+);
 
 has 'downloads' => (
     is => 'ro',
@@ -19,12 +25,6 @@ has 'executable' => (
 has 'config_yaml' => (
     is => 'ro',
     builder => '_read_config_yaml',
-    lazy => 1,
-);
-
-has 'executable_version' => (
-    is => 'ro',
-    builder => '_get_executable_version',
     lazy => 1,
 );
 
@@ -50,19 +50,6 @@ sub _read_config_yaml {
 sub _get_supported_executable_version {
     my ($self) = @_;
     return $self->config_yaml->{supported_version};
-}
-
-sub _get_executable_version {
-    my ($self) = @_;
-    my $observed_version = {};
-    my $executable = $self->executable;
-    my $output = $self->run("$executable -v");
-    my @matches = $output->{stderr} =~ 
-        m/\[debug\] youtube-dl version (?<year>\d\d\d\d).(?<month>\d\d).(?<day>\d\d).(?<release>\d+)/;
-    if (scalar @matches == 4) {
-        $observed_version->{$_} = shift @matches for qw/year month day release/;
-    }
-    return $observed_version;
 }
 
 sub get_downloads {
